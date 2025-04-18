dbf85f3a766b0bb114c60f9e70d3044ef80509d8
andrey <a.mironov95@mail.ru>
2025-04-18 10:24:46 +0300

    добавил документацию, конструкцию try/except в rabbit, исправил ошибку в названии файла инит

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -13,6 +13,19 @@ router = APIRouter()
 async def create_product(
     product: ProductCreate, session: AsyncSession = Depends(get_db)
 ):
+    """
+    Создает новый продукт в базе данных и отправляет информацию о нем в RabbitMQ.
+
+    **Параметры:**
+    - `product`: данные о новом продукте, которые должны включать название, цену и описание.
+    - `session`: асинхронная сессия базы данных для взаимодействия с SQLAlchemy.
+
+    **Ответ:**
+    - Возвращает созданный продукт в формате `ProductCreate` в случае успешного добавления.
+
+    **Ошибки:**
+    - Если продукт с таким названием уже существует, возвращается ошибка 400.
+    """
     result = await session.execute(select(Product).where(Product.name == product.name))
     existing = result.scalar_one_or_none()
     if existing:
@@ -27,13 +40,15 @@ async def create_product(
     await session.commit()
     await session.refresh(db_product)
 
-    await send_product_to_rabbit({
-        "message": "Product created",
-        "product": {
-            "name": db_product.name,
-            "price": db_product.price,
-            "description": db_product.description
+    await send_product_to_rabbit(
+        {
+            "message": "Product created",
+            "product": {
+                "name": db_product.name,
+                "price": db_product.price,
+                "description": db_product.description,
+            },
         }
-    })
+    )
 
     return db_product
--- app/consumer/consumer_send_product.py
+++ app/consumer/consumer_send_product.py
@@ -1,15 +1,31 @@
 import aio_pika
 import json
-import asyncio
 
 RABBITMQ_URL = "amqp://guest:guest@rabbitmq:5672/"
 EXCHANGE_NAME = "krakend_exchange"
 ROUTING_KEY = "product_created"
 
+
 async def send_product_to_rabbit(product: dict):
-    connection = await aio_pika.connect_robust(RABBITMQ_URL)
-    async with connection:
-        channel = await connection.channel()
-        exchange = await channel.declare_exchange(EXCHANGE_NAME, aio_pika.ExchangeType.DIRECT, durable=True)
-        message = aio_pika.Message(body=json.dumps(product).encode())
-        await exchange.publish(message, routing_key=ROUTING_KEY)
+    """
+    Отправляет данные о продукте в RabbitMQ.
+
+    **Параметры:**
+    - `product`: данные о продукте в виде словаря, который будет отправлен в очередь RabbitMQ.
+
+    **Процесс:**
+    - Устанавливается подключение к RabbitMQ.
+    - Создается обмен с типом `DIRECT`, в который будет отправлено сообщение.
+    - Сообщение сериализуется в JSON и отправляется в очередь с ключом маршрутизации.
+    """
+    try:
+        connection = await aio_pika.connect_robust(RABBITMQ_URL)
+        async with connection:
+            channel = await connection.channel()
+            exchange = await channel.declare_exchange(
+                EXCHANGE_NAME, aio_pika.ExchangeType.DIRECT, durable=True
+            )
+            message = aio_pika.Message(body=json.dumps(product).encode())
+            await exchange.publish(message, routing_key=ROUTING_KEY)
+    except Exception as e:
+        raise
